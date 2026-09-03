5dcccf73e179d5d39b5a7c156c51445ca7def85c
aki-13627 <aki.kaku0627@gmail.com>
2026-09-03 13:08:04 +0900

    refine: tuner

--- train_rtdetr.py
+++ train_rtdetr.py
@@ -3,6 +3,27 @@ from ultralytics import RTDETR
 def main():
     model = RTDETR("rtdetr-l.pt")
     
+    
+    search_space = {
+    
+        "lr0": (1e-5, 1e-2),
+        "lrf": (0.01, 1.0),
+        "box": (1.0, 10.0),
+        "cls": (0.5, 4.0),
+        "scale": (0.0, 0.15),
+        "degrees": (0.0, 10.0),
+        "mosaic": (0.0, 0.5),
+
+
+        "hsv_h": (0.0, 0.0),
+        "hsv_s": (0.0, 0.0),
+        "mixup": (0.0, 0.0),
+        "cutmix": (0.0, 0.0),
+        "flipud": (0.0, 0.0),
+        "perspective": (0.0, 0.0),
+        "shear": (0.0, 0.0)
+    }
+    
     model.tune(
         data="data.yaml",
         epochs=30,
@@ -10,8 +31,10 @@ def main():
         imgsz=768,
         batch=4,
         device=0,
+        optimizer="AdamW",
+        space=search_space,
         project="outputs_rtdetr",
-        name="tune_lung_nodule"
+        name="tune_lung_nodule_focused"
     )
 
 if __name__ == "__main__":
