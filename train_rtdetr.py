64a372336da76f5705033cefd86ebdc678b6587d
aki-13627 <aki.kaku0627@gmail.com>
2026-09-04 11:05:35 +0900

    change

--- train_rtdetr.py
+++ train_rtdetr.py
@@ -6,12 +6,11 @@ def main():
     results = model.train(
         data="data.yaml",
         epochs=100,
-        imgsz=768,
+        imgsz=512,
         batch=8,
         device=0,
         cls=2.5,
         box=5.0,
-        mosaic=1.0,
         project="outputs_rtdetr",
         name="lung_nodule_recall_run"
     )
