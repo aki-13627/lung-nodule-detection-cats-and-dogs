059001b0f52c7e35880b70b438ef851ac2c08461
aki-13627 <aki.kaku0627@gmail.com>
2026-09-09 16:26:20 +0900

    change

--- train_rtdetr.py
+++ train_rtdetr.py
@@ -12,8 +12,8 @@ def main():
         cls=2.5,
         box=5.0,
         mosaic=1.0,
-        project="outputs_rtdetrv4",
-        name="lung_nodule_recall_run"
+        project="outputs_rtdetr",
+        name="lung_nodule_recall_run-24"
     )
 
 if __name__ == "__main__":
